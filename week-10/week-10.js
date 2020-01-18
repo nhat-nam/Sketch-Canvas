c812e4fdcafb5e68e3c4ed54eab667b651f19d59
Nam Dao <nhatnamdao@gmail.com>
2020-01-18 16:09:48 +0700

    added week 10

--- week-10/week-10.js
+++ week-10/week-10.js
@@ -35,7 +35,7 @@ function Week10(){
     this.stars.push(s)
   }
   this.update = function(d){
-    if(this.ticks%100==0){
+    if(this.ticks%75==0){
       var f = new FireWork(Math.random()*500,500)
       this.fireworks.push(f)
     }
@@ -68,12 +68,19 @@ class FireWork{
   constructor(x, y){
     this.x = x
     this.y = y
-    this.dx = -25+Math.random()*50;
+
+
+
+    this.dx =  ((250-this.x)/150) *Math.random()*50;
     this.dy = -1 * Math.random()*100 - 100;
-    this.timer = Math.random()*150+20;
+
+    this.angle = Math.atanh(-1*this.dx/this.dy);
+
+    this.timer = 1000000;//Math.random()*150+20;
     this.ticks =0;
     this.radius = 10
-    this.color =
+    this.timer_started = false;
+    this.color = { red: 0, green: Math.random()*255, blue: Math.random()*255};
     this.explosion =null;
     this.has_exploded = false;
     this.img = new Image()
@@ -84,14 +91,22 @@ class FireWork{
       this.explosion.render(ctx);
     }else{
       ctx.save();
-      ctx.drawImage(this.img, this.x-20, this.y)
+      ctx.translate(this.x, this.y);
+      ctx.rotate(this.angle);
+      ctx.drawImage(this.img,0, 0);
       ctx.restore();
     }
   }
   update(d){
     this.ticks++;
-    if(this.ticks> this.timer && !this.has_exploded){
+    if(this.y< 200 && !this.timer_started){
+      this.ticks = 0;
+      this.timer = Math.random()*20+30;
+      this.timer_started = true;
+    }
+    if(this.ticks > this.timer && !this.has_exploded){
       this.explosion = new Explosion(this.x, this.y);
+      this.explosion.color= this.color;
       this.has_exploded = true;
     }
     if(this.explosion){
@@ -111,6 +126,11 @@ class Explosion{
     this.particles =[];
     this.ticks = 0
     this.radius = 40
+    this.color = {
+      red: 0,
+      green: 0,
+      blue: 200
+    };
 
   }
   render(ctx){
@@ -130,11 +150,14 @@ class Explosion{
     this.ticks++
     if(this.ticks > 2 && this.ticks<4){
       for(var i=0;i<70;i++){
-          this.particles.push(new Particle(this.x, this.y));
+          this.particles.push(new Particle(this.x, this.y, this.color));
       }
     }
     for(var i=0;i<this.particles.length;i++){
       this.particles[i].update(d);
+      if(this.particles[i].delete){
+        this.particles.splice(i,1);
+      }
     }
   }
 }
@@ -142,38 +165,46 @@ class Explosion{
 
 
 class Particle{
-  constructor(x,y){
+  constructor(x,y, color){
     this.x = x;
     this.y = y;
-    this.ticks =0;
+    this.ticks = 0;
     this.dx = -150 + Math.random()*300
     this.dy = -150 + Math.random()*300
-
+    this.color = color;
     this.trail = [];
     this.is_trail = false;
     this.delete = false;
     this.opacity = 1;
+
+
   }
   render(ctx){
-
-    ctx.fillStyle="rgba(0,0,200,"+this.opacity+")";
+    ctx.save();
+    ctx.fillStyle="rgba("+this.color.red+","+this.color.green+","+this.color.blue+","+this.opacity+")";
     ctx.fillRect(this.x, this.y, 5,5);
     for(var i=0;i<this.trail.length;i++){
       if(!this.trail[i].delete)
         this.trail[i].render(ctx);
     }
+    ctx.restore();
   }
   update(d){
     this.ticks++;
     if(this.ticks % 1 == 0 && !this.is_trail){
       var x = this.x;
       var y = this.y;
-      var p = new Particle(x,y);
+      var p = new Particle(x,y, this.color);
       p.dx = 0;
       p.dy = 0;
       p.opacity = this.opacity
       p.is_trail = true;
       this.trail.push(p);
+      this.opacity-=.01*Math.random();
+
+      if(this.opacity < .05){
+        this.delete = true;
+      }
     }else if(this.is_trail && this.ticks > 30){
       this.delete = true;
     }else if(this.is_trail){
@@ -182,15 +213,17 @@ class Particle{
 
       this.opacity-=.01*Math.random();
     }
+
     this.dy = this.dy + 150*d/1000;
     this.x = this.x + this.dx*d/1000;
     this.y = this.y + this.dy*d/1000;
     for(var i=0;i<this.trail.length;i++){
-      this.trail[i].update;
+      this.trail[i].update(d);
       if(this.trail[i].delete){
         this.trail.splice(i,1);
       }
     }
+
   }
 }
 class Star{
